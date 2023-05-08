4e204c0325eb96e7fb152b6f8189fa1abaa84ccf
Gratiela Uivari <gratiela.uivari@indsoft.ro>
2023-05-08 20:50:58 +0300

    cange ts config

--- src/index.tsx
+++ src/index.tsx
@@ -4,29 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// const root = ReactDOM.createRoot(
-//   document.getElementById('root') as HTMLElement
-// );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-//@ts-ignore
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  root.render(element);
-}
-
-setInterval(tick, 1000);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/test.typescript.ts
@@ -0,0 +1,12 @@
+function getConf() {
+  return {
+    name: "my conf",
+    x: 123,
+    ready: true
+  };
+}
+
+// const conf = getConf();
+//console.info(conf.nume);
+
+//conf.ready = "not ready";
