28e6fc096104d4c782052b7db529f4467d5e33c1
Gratiela Uivari <gratiela.uivari@indsoft.ro>
2023-05-10 17:59:27 +0300

    first version of cv react

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./pictures/pozaprofil.jpg";
+import linkedin from "./pictures/linkedin.png";
+import git from "./pictures/github.png";
+import youtube from "./pictures/youtube.png";
+import send from "./pictures/send.png";
 import "./App.css";
 
 function AppHeader() {
   return (
-    <div>
-      <button id="removeSelected">Remove selected</button>
-      <input type="search" name="search" id="search" />
-    </div>
+    <header>
+      <div id="header-wrapper">
+        <div>
+          <img src={logo} alt="poza" id="profil-picture" height="100" />
+        </div>
+        <div>
+          <h1>Gratiela Uivari</h1>
+          <h2 id="job-title">Programator</h2>
+        </div>
+      </div>
+      <MainMenu />
+    </header>
   );
 }
 
@@ -52,7 +64,7 @@ function SideMenu() {
           <textarea></textarea>
           <div id="send-button-div">
             <button type="submit" id="send-button">
-              <img src="pictures/send.png" width="16px" height="16px" />
+              <img src={send} width="16px" height="16px" />
               <b>SEND</b>
             </button>
           </div>
@@ -62,13 +74,98 @@ function SideMenu() {
     </div>
   );
 }
+
+function AppFooter() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/gratiela-uivari-651336270/" target="_blank">
+            <img height="30px" width="60px" src={linkedin} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/" target="_blank">
+            <img height="30px" src={youtube} />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/gratielau" target="_blank">
+            <img height="30px" src={git} />
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+function ContentWrapper() {
+  return (
+    <section id="content">
+      <SideMenu></SideMenu>
+      <div id="main">
+        <div className="page" id="home">
+          Home content ...
+        </div>
+        <div className="page" id="skills">
+          <h2>Skills & Endorcements</h2>
+          <ul></ul>
+        </div>
+        <div className="page" id="projects">
+          <div className="container">
+            <table className="text-align: center">
+              <tr>
+                <td>
+                  <h2>Your score:</h2>
+                </td>
+                <td></td>
+                <td>
+                  <h2>Time Left:</h2>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h2 id="score">0</h2>
+                </td>
+                <td>
+                  <button type="button" id="btnStart">
+                    Start
+                  </button>
+                </td>
+                <td>
+                  <h2 id="time-left">30</h2>
+                </td>
+              </tr>
+            </table>
+
+            <div className="grid">
+              <div className="square" id="1"></div>
+              <div className="square" id="2"></div>
+              <div className="square" id="3"></div>
+              <div className="square" id="4"></div>
+              <div className="square" id="5"></div>
+              <div className="square" id="6"></div>
+              <div className="square" id="7"></div>
+              <div className="square" id="8"></div>
+              <div className="square" id="9"></div>
+            </div>
+          </div>
+        </div>
+        <div className="page" id="languages">
+          Languages content...
+        </div>
+      </div>
+    </section>
+  );
+}
+
 function App() {
   return (
-    <div>
+    <>
       <AppHeader />
-      <MainMenu />
-      <SideMenu />
-    </div>
+      <ContentWrapper />
+      <AppFooter />
+    </>
   );
 }
 function App_old() {
